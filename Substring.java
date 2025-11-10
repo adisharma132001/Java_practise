f3583da3f0b8ba3c63b3ef09b582ae015087cffc
Aditya Sharma <adisharma132001@gmail.com>
2025-11-10 17:28:59 +0530

    Day 3 ka first question

---
+++ Substring.java
@@ -0,0 +1,10 @@
+public class Substring {
+    public static void main(String[] args) {
+        String str1="Java";
+        String str2 ="This is the Java";
+
+        System.out.println(str2.contains(str1));
+        
+
+    }
+}
---
+++ Two_sum.java
@@ -0,0 +1,20 @@
+public class Two_sum {
+    public static void main(String[] args) {
+        int arr[]= {1,2,3,4,5};
+        int sum_arr[]={};
+        int target=5;
+
+        int a1=0;
+        int a2=arr[0];
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]+arr[i+1]==target){
+                
+            }
+        }
+
+        
+    }
+
+    
+}
